3de2a5dd6342876779d10d508d9b67f116255ad9
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-30 21:13:05 +0300

    01 Revision

--- Demo_OOP03/Program.cs
+++ Demo_OOP03/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace Demo_OOP03
+{
+
+    // DataTypes that could create in name space
+    // 1.Class
+    // 2.Struct
+    // 3.Enum
+    // 4.Interface 
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
