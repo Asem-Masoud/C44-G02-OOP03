35bf6c53b2bc75e7f516019ee2b179c242d210f5
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-31 02:31:24 +0300

    Questions

--- Ass_OOP03/Program.cs
+++ Ass_OOP03/Program.cs
@@ -5,6 +5,60 @@
         static void Main()
         {
 
+            #region Part01 MCQ
+
+            /*
+              
+            1. ( )
+            
+            2. ( )
+
+            3. ( )
+
+            4. ( )
+
+            5. ( )
+
+            6. ( )
+
+            7. ( )
+
+            8. ( )
+
+            9. ( )
+
+            10. ( )
+
+
+            */
+
+            #endregion
+
+            #region Q1P02
+            /*
+             Define an interface named IShape with a property Area and a method DisplayShapeInfo. Create two interfaces, ICircle and IRectangle, that inherit from IShape. Implement these interfaces in classes Circle and Rectangle. Test your implementation by creating instances of both classes and displaying their shape information. 
+             */
+
+
+
+            #endregion
+
+
+            #region Q2P02
+
+
+
+            #endregion
+
+
+            #region Q3P02
+
+
+
+
+            #endregion
+
+
         }
     }
 }
