ff01aabce9d66529396e13841e90f1f6cb1dc8ed
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-30 21:26:59 +0300

    02 Interface Ex01

---
+++ Demo_OOP03/Interface Ex01/MyType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OOP03.Interface_Ex01
+{
+    internal class MyType : IMyType
+    {
+
+        #region 02 Interface Ex01
+
+        //  automatic Properity
+        public double Salary { get; set; }
+        //public double Salary
+        //{
+        //    get { throw new NotImplementedException(); }
+        //    set { throw new NotImplementedException(); }
+
+
+        //}
+
+        public void MyFun()
+        {
+            Console.WriteLine("Hello World");
+        }
+        #endregion
+
+    }
+}
--- Demo_OOP03/Program.cs
+++ Demo_OOP03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo_OOP03
+﻿using Demo_OOP03.Interface_Ex01;
+
+namespace Demo_OOP03
 {
 
     // DataTypes that could create in name space
@@ -11,7 +13,29 @@
     {
         static void Main()
         {
+            #region 02 Interface Ex01
 
+            // Interface : One of the dataTypes that could create in name space
+            //Code Contract between the developer who write and the developer who use
+
+            //Note : u can't Create Object from Interface
+
+            //Mytype mytype = new Mytype();
+            //mytype.Salary = 12000;
+            //Console.WriteLine(mytype.Salary);
+            //mytype.MyFun();
+
+
+            //Note U can create any ref throw interface
+
+            IMyType myType;
+            //Ref --> Can refer to Object From any Class which implement the interface"IMytype"
+            MyType mytype = new MyType();
+            mytype.Salary = 12000;
+            Console.WriteLine(mytype.Salary);
+            mytype.MyFun();
+
+            #endregion
         }
     }
 }
